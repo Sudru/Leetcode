82281ad6ff0d899f0be3e48b455dc6b2fae4d421
Sudarshan <sudarshandevkota22@gmail.com>
2022-10-19 11:07:50 +0545

    same tree

--- Search2DMatrix74.java
+++ Search2DMatrix74.java
@@ -1,7 +1,6 @@
 public class Search2DMatrix74 {
     public static void main(String[] args) {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        int[][] m2 = {{1},{3},{5}};
         System.out.println(searchMatrix(matrix,30));
     }
 
