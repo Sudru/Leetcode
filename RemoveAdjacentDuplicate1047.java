c64123d23efd2c0bdb8d9b6a347e4ce1b74132e7
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-10 20:23:37 +0545

    remove adjacent duplicate

---
+++ RemoveAdjacentDuplicate1047.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+public class RemoveAdjacentDuplicate1047{
+	public static void main(String[] args){
+		System.out.println(removeDuplicates("azxxzy"));
+	}
+	public static String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(stack.isEmpty() || stack.peek()!=s.charAt(i)){
+            	stack.push(s.charAt(i));
+            }else{
+                stack.pop();
+            }
+        }
+        
+       return stack.stream().map(a->a.toString()).collect(Collectors.joining(""));
+    }
+
+}
